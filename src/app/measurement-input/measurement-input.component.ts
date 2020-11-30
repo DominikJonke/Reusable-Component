cecf0836318d4148a82498f7f4759d0f46b10a3b
JD <65065236+D0mnik@users.noreply.github.com>
2020-11-30 11:12:12 +0100

    bug fix

--- src/app/measurement-input/measurement-input.component.ts
+++ src/app/measurement-input/measurement-input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 import { Measurement } from '../class/measurement-class';
 
 @Component({
@@ -12,8 +13,8 @@ export class MeasurementInputComponent implements OnInit {
   @Input() units = ['°C', '%', 'ppm'];
 
 
-  @Output() itemsChange = new EventEmitter<Measurement>();
-  @Output() unitChange = new EventEmitter<Measurement>();
+  //@Output() itemsChange = new EventEmitter<Measurement>();
+  //@Output() unitChange = new EventEmitter<Measurement>();
   
   constructor() { }
 
